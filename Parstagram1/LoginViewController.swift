de5246820d01d3a9f356eab1ee42e2279cc7bd4c
Aniya <amallen046@gmail.com>
2019-11-16 00:46:00 -0600

    .

--- Parstagram1/LoginViewController.swift
+++ Parstagram1/LoginViewController.swift
@@ -7,14 +7,54 @@
 //
 
 import UIKit
+import Parse
 
 class LoginViewController: UIViewController {
 
+    
+    @IBOutlet weak var usernameField: UITextField!
+    
+    
+    @IBOutlet weak var passwordField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func onSignIn(_ sender: Any) {
+        let user = PFUser()
+        user.username = usernameField.text
+        user.password = passwordField.text
+        
+       // user = signUpInBackground(block:)
+        user.signUpInBackground { (success, error) in
+            if success {
+                self.performSegue(withIdentifier: "loginSegue", sender: nil)
+            }else{
+                print("Error: \(error?.localizedDescription)")
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func onSignUp(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 
     /*
